ab114f3a2f55cd2547e4ec64b13ff4d6b206738c
Seanforfun <32276204+Seanforfun@users.noreply.github.com>
2018-04-13 19:34:07 -0400

    [Function add]
    1.Intergrate bootstrap into this project.

--- src/main/java/ca/seanforfun/blog/SeanforfunBlogApplication.java
+++ src/main/java/ca/seanforfun/blog/SeanforfunBlogApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SeanforfunBlogApplication {
+public class SeanForFunBlogApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SeanforfunBlogApplication.class, args);
+		SpringApplication.run(SeanForFunBlogApplication.class, args);
 	}
 }
