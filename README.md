b2f6801b80230cca080d0e197ebc7dc3d5479fa3
Seanforfun <32276204+Seanforfun@users.noreply.github.com>
2018-04-13 11:44:00 -0400

    [Function add]
    1.Use spring boot to create the basic frame of blog.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # SeanForFunBlog
-A self-design blog.
+This is the source code of project SeanforfunBlog. This is a java web based technology blog.
---
+++ src/main/java/ca/seanforfun/blog/SeanforfunBlogApplication.java
@@ -0,0 +1,12 @@
+package ca.seanforfun.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeanforfunBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeanforfunBlogApplication.class, args);
+	}
+}
