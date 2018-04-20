d13e639fceec1b8a8922e9892d5b8f1b766d3225
SeanForFun <xiaob6@mcmaster.ca>
2018-04-20 10:56:26 -0400

    [Function add]
    1.Add saved front categories into application context. Need to be tested.

--- src/main/java/ca/seanforfun/blog/config/runner/CategoryRunner.java
+++ src/main/java/ca/seanforfun/blog/config/runner/CategoryRunner.java
@@ -4,8 +4,12 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -15,16 +19,23 @@ import ca.seanforfun.blog.service.ebo.CategoryService;
 @Component
 @Order(value=1)
 public class CategoryRunner implements CommandLineRunner {
+	private static Map<Category, List<Category>> frontCategoryMap = null;
+	
 	@Autowired
 	private CategoryService categoryService;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		List<Category> primaryFrontCategories = categoryService.getPrimaryFrontCategories();
-		Map<Category, List<Category>> categoryMap = new LinkedHashMap<>();
+		frontCategoryMap = new LinkedHashMap<>();
 		for(Category c:primaryFrontCategories){
 			List<Category> secondaryCategory = categoryService.getSecondaryCategoriesByPid(c.getId());
-			categoryMap.put(c, secondaryCategory);
+			frontCategoryMap.put(c, secondaryCategory);
 		}
 	}
-
+	
+	@Bean
+	public Map<Category, List<Category>> categoryMap(){
+		return frontCategoryMap;
+	}
 }
